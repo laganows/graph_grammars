2a4d45eb1ecf90dd9d207a293235d5440217d0bd
Grzegorz Laganowski <g.laganowski94@gmail.com>
2017-05-12 20:58:01 +0200

    Added changes in order to pull request review

--- src/main/java/pl/edu/agh/ki/gg/pt1250/Executor.java
+++ src/main/java/pl/edu/agh/ki/gg/pt1250/Executor.java
@@ -10,7 +10,7 @@ import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 class Executor extends Thread {
-    private final double BASIC_UNIT_LENGTH = 4.0;
+    private static final double BASIC_UNIT_LENGTH = 4.0;
 
     public synchronized void run() {
         try {
@@ -21,22 +21,22 @@ class Executor extends Thread {
             p1.start();
             barrier.await();
 
-            Visualizer visualizer = new Visualizer();
-            visualizer.visualizeGraph(s);
-            visualizer.displayGraph();
-
             //[(P2)]
             barrier = new CyclicBarrier(2);
             P2 p2 = new P2(s, barrier, BASIC_UNIT_LENGTH/2);
             p2.start();
             barrier.await();
 
-            Visualizer visualizer2 = new Visualizer();
-            visualizer2.visualizeGraph(s);
-            visualizer2.displayGraph();
+            displayGraph(s);
 
         } catch (InterruptedException | BrokenBarrierException e) {
             e.printStackTrace();
         }
+    }
+
+    private void displayGraph(Vertex startVertex) {
+        Visualizer visualizer = new Visualizer();
+        visualizer.visualizeGraph(startVertex);
+        visualizer.displayGraph();
     }
 }
