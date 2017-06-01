cff4d600c57ef66b1b4f324728e6eb7db89a2a3f
Tomasz Kasprzyk <kasptom@gmail.com>
2017-06-01 19:26:51 +0200

    Use images instead of labels

---
+++ src/main/java/pl/edu/agh/ki/gg/pt1250/visualization/EmptyMouseManager.java
@@ -0,0 +1,46 @@
+package pl.edu.agh.ki.gg.pt1250.visualization;
+
+import org.graphstream.ui.graphicGraph.GraphicGraph;
+import org.graphstream.ui.view.View;
+import org.graphstream.ui.view.util.MouseManager;
+
+import java.awt.event.MouseEvent;
+
+public class EmptyMouseManager implements MouseManager {
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void init(GraphicGraph graph, View view) {
+    }
+
+    @Override
+    public void release() {
+    }
+}
